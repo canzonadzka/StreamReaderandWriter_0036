c8ec1a02ae795d92f5e929232c8a053e827f65d4
canzonadzka <canzonadzka@gmail.com>
2026-06-01 10:05:58 +0700

    membuat kode untuk menunjuk ke sebuah nama file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -9,3 +9,7 @@ int main()
     string baris;
     //membuka file dalam mode menulis
     ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
